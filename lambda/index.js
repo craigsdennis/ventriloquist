c20ccff019bdb641685c94734b088a61aac8001b
craigsdennis <craig@teamtreehouse.com>
2017-06-23 01:04:40 -0700

    Caches per session

--- lambda/index.js
+++ lambda/index.js
@@ -2,19 +2,24 @@
 
 const Alexa = require('alexa-sdk');
 const https = require('https');
+const util = require('util');
 
 const APP_ID = 'amzn1.ask.skill.37c38d6c-cbbb-41de-9ec3-9c09c6cf78a1';
 const SCRIPT_URL = 'https://raw.githubusercontent.com/craigsdennis/ventriloquist/master/docs/main.ssml';
 
-function getLines(callback) {
+function getScript(attributes, callback) {
+    // Cache this per session
+    if (attributes.dummyScript !== undefined) {
+        return callback(null, attributes.dummyScript);
+    }
     var request = https.get(SCRIPT_URL, (res) => {
         let data = '';
         res.on('data', function (chunk) {
             data += chunk;
         });
         res.on('end', function () {
-            let lines = data.split(/\r?\n/);
-            callback(null, lines.filter(line => line.length > 0));
+            attributes.dummyScript = data;
+            callback(null, data);
         });
     });
     request.on('error', function (e) {
@@ -33,18 +38,25 @@ const handlers = {
     },
     'GetDummyLine': function () {
         // Attributes are session based
-        getLines((err, lines) => {
+        getScript(this.attributes, (err, data) => {
             if (err) {
                 this.emit(':tell', 'Something went really wrong: ' + err.message);
                 throw err;
             }
+            // Make an array based on newlines
+            let lines = data.split(/\r?\n/);
+            lines = lines.filter(line => line.length > 0);
             let lineIndex = this.attributes.lineIndex;
             if (lineIndex === undefined) {
                 lineIndex = -1;
             }
             lineIndex++;
             this.attributes.lineIndex = lineIndex;
-            const line = lines[lineIndex];
+            if (lineIndex > lines.length) {
+                this.emit(':tell', 'See ya next time!');
+            }
+            const whatevs = this.event.request.intent.slots.Whatever.value;
+            const line = lines[lineIndex].replace('${whatevs}', whatevs);
 
             // Create speech output
             this.emit(':ask', line);
