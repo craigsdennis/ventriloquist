0873df8b730ea1df3bdd8ac10281990bd01fdf0e
craigsdennis <craig@teamtreehouse.com>
2017-06-24 01:00:24 -0700

    Adds missing callback param

--- lambda/index.js
+++ lambda/index.js
@@ -77,8 +77,8 @@ const handlers = {
     },
 };
 
-exports.handler = function (event, context) {
-    const alexa = Alexa.handler(event, context);
+exports.handler = function (event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
     alexa.appId = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
