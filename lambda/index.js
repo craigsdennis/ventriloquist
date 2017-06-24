c451176ce6b6b40034316db13cef55641ce04191
craigsdennis <craig@teamtreehouse.com>
2017-06-24 00:56:48 -0700

    Removes failed attempt at list management

--- lambda/index.js
+++ lambda/index.js
@@ -6,167 +6,32 @@ const https = require('https');
 const APP_ID = 'amzn1.ask.skill.b3a058bd-4db2-4da6-9189-139ca25688e7';
 const SCRIPT_URL = 'https://raw.githubusercontent.com/craigsdennis/ventriloquist/master/docs/main.ssml';
 
-/**
- * List API end-point.
- */
-// read::alexa:household:list
-const API_URL = 'api.amazonalexa.com';
-const API_PORT = '443';
 
-
-/**
- * List API to retrieve the List of Lists : Lists Metadata.
- */
-const getListsMetadata = function(session, callback) {
-    if(!session.user.permissions) {
-        console.log("permissions are not defined");
-        callback(null);
-        return;
-    }
-    const consent_token = session.user.permissions.consentToken;
-    console.log("Starting the get list metadata call.");
-    const options = {
-        host: API_URL,
-        port: API_PORT,
-        path: '/v2/householdlists/',
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + consent_token,
-            'Content-Type': 'application/json'
-        }
-    };
-
-    https.request(options, (res) => {
-        console.log('STATUS: ', res.statusCode);
-        console.log('HEADERS: ', JSON.stringify(res.headers));
-
-        if(res.statusCode === 403) {
-            console.log("permissions are not granted");
-            callback(null);
-            return;
-        }
-
-        var body = [];
-        res.on('data', function(chunk) {
-            body.push(chunk);
-        }).on('end', function() {
-            body = Buffer.concat(body).toString();
-            callback(body);
+function getScript(callback) {
+    const request = https.get(SCRIPT_URL, (res) => {
+        let data = '';
+        res.on('data', function (chunk) {
+            data += chunk;
         });
-
-        res.on('error', (e) => {
-            console.log(`Problem with request: ${e.message}`);
+        res.on('end', function () {
+            callback(null, data);
         });
-    }).end();
-};
-
-const getToDoList = function(session, callback) {
-    if(!session.user.permissions) {
-        console.log("permissions are not defined");
-        callback(null);
-        return;
-    }
-    const consent_token = session.user.permissions.consentToken;
-    console.log("Starting get todo list call.");
-
-    getListsMetadata(session, function(returnValue) {
-        if(!returnValue) {
-            console.log("permissions are not defined");
-            callback(null);
-            return;
-        }
-        const obj = JSON.parse(returnValue);
-        let todo_path = '';
-        for (let i = 0; i < obj.lists.length; i++) {
-            if (obj.lists[i].name === 'Alexa to-do list') {
-                for (let j = 0; j < obj.lists[i].statusMap.length; j++) {
-                    if (obj.lists[i].statusMap[j].status === 'active') {
-                        todo_path = obj.lists[i].statusMap[j].href;
-                        break;
-                    }
-                }
-                break;
-            }
-        }
-
-        const options = {
-            host: API_URL,
-            port: API_PORT,
-            path: todo_path,
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer ' + consent_token,
-                'Content-Type': 'application/json'
-            }
-        };
-
-        https.request(options, (res) => {
-            console.log('STATUS: ', res.statusCode);
-            console.log('HEADERS: ', JSON.stringify(res.headers));
-            if(res.statusCode === 403) {
-                console.log("permissions are not granted");
-                callback(null);
-                return;
-            }
-            const body = [];
-            res.on('data', function(chunk) {
-                body.push(chunk);
-            }).on('end', function() {
-                const bodyBuffer = Buffer.concat(body).toString();
-                callback(JSON.parse(bodyBuffer));
-            });
-            res.on('error', (e) => {
-                console.log(`Problem with request: ${e.message}`);
-            });
-        }).end();
     });
-};
-
-function getScript(session, callback) {
-    getToDoList(session, (todos) => {
-        console.log("Todos are " + todos);
-        const request = https.get(SCRIPT_URL, (res) => {
-            let data = '';
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-            res.on('end', function () {
-                callback(null, data);
-            });
-        });
-        request.on('error', function (e) {
-            callback(e, null);
-        });
-        request.end();
+    request.on('error', function (e) {
+        callback(e, null);
     });
-
+    request.end();
 }
 
 const handlers = {
     'LaunchRequest': function () {
         this.emit('GetDummyLine');
     },
-    'Unhandled': function() {
-        this.emit(':tellWithPermissionCard',
-            'In order to use this app you need to grant permissions to your todo list',
-            ['read::alexa:household:list']
-        );
-    },
     'GetNewDummyLineIntent': function () {
         this.emit('GetDummyLine');
     },
     'GetDummyLine': function () {
-        console.log('User info is ', this.event.session.user);
-        const permissions = this.event.session.user.permissions;
-
-        if (permissions === undefined ||
-                permissions.consentToken === undefined) {
-            this.emit(':tellWithPermissionCard',
-                'In order to use this app you need to grant permissions to your todo list',
-                ['read::alexa:household:list']
-            );
-        }
-        getScript(this.event.session, (err, data) => {
+        getScript((err, data) => {
             if (err) {
                 this.emit(':tell', 'Something went really wrong: ' + err.message);
                 throw err;
