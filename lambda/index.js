f10a600baa0699b456693f447334cb693000ac22
craigsdennis <craig@teamtreehouse.com>
2017-06-29 22:37:02 -0700

    Adds simple DynamoDB setup

--- lambda/index.js
+++ lambda/index.js
@@ -80,6 +80,7 @@ const handlers = {
 exports.handler = function (event, context, callback) {
     const alexa = Alexa.handler(event, context, callback);
     alexa.appId = APP_ID;
+    alexa.dynamoDBTableName = 'DummyScripts';
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
